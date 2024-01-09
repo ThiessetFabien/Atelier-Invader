6e946bdf64ade2b186f172d0f102f9e8b0990c25
Fabien Thiesset <thiessetfabien3@gmail.com>
2024-01-09 14:16:19 +0100

    feet/pixel clic

--- js/app.js
+++ js/app.js
@@ -1,21 +1,50 @@
 
 
- const createInvader = () => {
+//  const createInvader = () => {
+//     const mainDiv = document.createElement('div');
+//     mainDiv.classList.add('mainDiv')
+//     for (let i = 0; i < 8; i++) {
+//         const colDiv = document.createElement('div');
+//         colDiv.classList.add('colDiv')
+//         mainDiv.appendChild(colDiv);
+
+//         for (let j = 0; j<8; j++) {
+//         const rowDiv = document.createElement('div')
+//         rowDiv.classList.add('rowDiv')
+//         colDiv.appendChild(rowDiv);
+//         }
+//     }
+//     return mainDiv;
+//  };
+
+// document.getElementById('invader').appendChild(createInvader());
+
+
+
+const createInvader = () => {
     const mainDiv = document.createElement('div');
-    mainDiv.classList.add('mainDiv')
+    mainDiv.classList.add('mainDiv');
+
     for (let i = 0; i < 8; i++) {
         const colDiv = document.createElement('div');
-        colDiv.classList.add('colDiv')
+        colDiv.classList.add('colDiv');
         mainDiv.appendChild(colDiv);
 
-        for (let j = 0; j<8; j++) {
-        const rowDiv = document.createElement('div')
-        rowDiv.classList.add('rowDiv')
-        colDiv.appendChild(rowDiv);
+        for (let j = 0; j < 8; j++) {
+            const rowDiv = document.createElement('div');
+            rowDiv.classList.add('rowDiv');
+           
+            rowDiv.addEventListener('click', function() {
+                if (this.style.backgroundColor === 'black') {
+                   this.style.backgroundColor = '#d2dae2';
+                }else {
+                    this.style.backgroundColor = 'black';
+                }
+               });
+            colDiv.appendChild(rowDiv); 
         }
     }
     return mainDiv;
- };
+};
 
-document.getElementById('invader').appendChild(createInvader());
-
+document.getElementById('invader').appendChild(createInvader()); 
