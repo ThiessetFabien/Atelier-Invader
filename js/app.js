c6953e7a1d5e8baa579daa3a2803bdc85a0ec262
Fabien Thiesset <thiessetfabien3@gmail.com>
2024-01-09 13:12:51 +0100

    feet/layout

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-// ton code ici
+
+
+ const createInvader = () => {
+    const mainDiv = document.createElement('div');
+    mainDiv.classList.add('mainDiv')
+    for (let i = 0; i < 8; i++) {
+        const colDiv = document.createElement('div');
+        colDiv.classList.add('colDiv')
+        mainDiv.appendChild(colDiv);
+
+        for (let j = 0; j<8; j++) {
+        const rowDiv = document.createElement('div')
+        rowDiv.classList.add('rowDiv')
+        colDiv.appendChild(rowDiv);
+        }
+    }
+    return mainDiv;
+ };
+
+document.getElementById('invader').appendChild(createInvader());
+
