07c0a999bac4c84eebe57f7f65cab8d917539732
Fabien Thiesset <thiessetfabien3@gmail.com>
2024-01-09 17:16:47 +0100

    feet/grid size

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ label1.classList.add('setting-gridNumber')
 
 input1.type = 'text';
 input1.name = 'setting-gridNumber';
-input1.id = 'gridNumber';
+input1.id = 'grid-number';
 input1.placeholder = "Taille de la grille";
 
 createGrid.appendChild(label1);
@@ -33,7 +33,7 @@ input2.id = "pixelNumber"
 input2.placeholder = 'Taille des pixels'; 
 
 createGrid.appendChild(label2);
-label2.appendChild(input2)
+label2.appendChild(input2);
 
 //bouton valider
 
@@ -47,19 +47,39 @@ input3.value = 'Valider';
 
 
 createGrid.appendChild(label3);
-label2.appendChild(input3)
+label3.appendChild(input3);
 
+// récupération des valeurs 
+document.getElementsByTagName('form')[0].addEventListener('submit', function (event) 
+{
+    event.preventDefault();
+    
+    let input1Value = document.getElementById('grid-number').value;
 
-const createInvader = () => {
+    if (!isNaN(input1Value)) 
+    {
+        input1Value = parseInt(input1Value);
+        const container = document.getElementById('invader');
+        container.innerHTML = '';
+        const invaderDiv = createInvader(input1Value);
+        container.appendChild(invaderDiv);
+    }
+    else
+    {
+        console.error('Veuillez entrer un nombre valide.')
+    }
+})
+
+const createInvader = (input1Value = 8) => {
     const mainDiv = document.createElement('div');
     mainDiv.classList.add('mainDiv');
 
-    for (let i = 0; i < 8; i++) {
+    for (let i = 0; i < input1Value; i++) {
         const colDiv = document.createElement('div');
         colDiv.classList.add('colDiv');
         mainDiv.appendChild(colDiv);
 
-        for (let j = 0; j < 8; j++) {
+        for (let j = 0; j < input1Value; j++) {
             const rowDiv = document.createElement('div');
             rowDiv.classList.add('rowDiv');
            
@@ -77,4 +97,7 @@ const createInvader = () => {
 };
 
 document.getElementById('invader').appendChild(createInvader()); 
-
+const container = document.getElementById('invader');
+container.innerHTML = '';
+const invaderDiv = createInvader();
+container.appendChild(invaderDiv);
