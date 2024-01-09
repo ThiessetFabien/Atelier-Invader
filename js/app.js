bc822c04ce9929609f3a3425a9bb12344f57e6f2
Fabien Thiesset <thiessetfabien3@gmail.com>
2024-01-09 14:17:37 +0100

    --amend

--- js/app.js
+++ js/app.js
@@ -1,26 +1,3 @@
-
-
-//  const createInvader = () => {
-//     const mainDiv = document.createElement('div');
-//     mainDiv.classList.add('mainDiv')
-//     for (let i = 0; i < 8; i++) {
-//         const colDiv = document.createElement('div');
-//         colDiv.classList.add('colDiv')
-//         mainDiv.appendChild(colDiv);
-
-//         for (let j = 0; j<8; j++) {
-//         const rowDiv = document.createElement('div')
-//         rowDiv.classList.add('rowDiv')
-//         colDiv.appendChild(rowDiv);
-//         }
-//     }
-//     return mainDiv;
-//  };
-
-// document.getElementById('invader').appendChild(createInvader());
-
-
-
 const createInvader = () => {
     const mainDiv = document.createElement('div');
     mainDiv.classList.add('mainDiv');
